8a10a19b7cb9e096b8bb4955c7562033bfdcdb9c
Cai Lize <cail@ma.makino.biz>
2019-05-24 16:35:35 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # SentimentAnalysisFunctionsApp
 SentimentAnalysisFunctionsApp, deployed by azure function, a demo
+
+# how to test
+
+```
+power shell
+$Invoke-RestMethod "http://localhost:7071/api/AnalyzeSentiment" -Method Post -Body (@{SentimentText="This is a very bad steak"} | ConvertTo-Json) -ContentType "application/json"
+
+content:
+{
+    "SentimentText":  "This is a ver\ny bad steak"
+}
+
+```
---
+++ SentimentAnalysisFunctionsApp/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.ML;
+using SentimentAnalysisFunctionsApp;
+using SentimentAnalysisFunctionsApp.DataModels;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+namespace SentimentAnalysisFunctionsApp
+{
+    class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            builder.Services.AddPredictionEnginePool<SentimentData, SentimentPrediction>()
+                .FromFile("MLModels/sentiment_model.zip");
+        }
+    }
+}
